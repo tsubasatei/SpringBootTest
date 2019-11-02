87d2f83e25e2f0109855c09bdaa5ac7f8ef13a6d
tsubasatei <tsubasa_337283708@qq.com>
2019-11-02 16:57:15 +0800

    自动配置原理

--- src/main/java/com/xt/springboot/SpringBootTestApplication.java
+++ src/main/java/com/xt/springboot/SpringBootTestApplication.java
@@ -9,6 +9,14 @@ import org.springframework.context.annotation.ImportResource;
  *
  * @ImportResource(locations = {"classpath:beans.xml"}) :
  *      导入 Spring 的配置文件，让配置文件里面的内容生效，标注在配置类上，不推荐这种写法
+ *
+ * springboot 启动会扫描以下位置的 application.properties 或者 application.yml 文件作为 Spring boot 的默认配置文件
+ *      项目路径下:  –file:./config/
+ *                  –file:./
+ *      类路径下：   –classpath:/config/
+ *                  –classpath:/
+ *      优先级由高到底，高优先级的配置会覆盖低优先级的配置；
+ *      SpringBoot会从这四个位置全部加载主配置文件；互补配置；
  */
 //@ImportResource(locations = {"classpath:beans.xml"})
 @SpringBootApplication
