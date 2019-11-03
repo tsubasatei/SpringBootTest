ab88fda3ee1ae248489a4a86637dfd4bbeeca7ba
tsubasatei <tsubasa_337283708@qq.com>
2019-11-03 11:24:04 +0800

    日志框架self4j + logback 切换 log4j2 或 log4j

--- src/test/java/com/xt/springboot/SpringBootTestApplicationTests.java
+++ src/test/java/com/xt/springboot/SpringBootTestApplicationTests.java
@@ -2,6 +2,8 @@ package com.xt.springboot;
 
 import com.xt.springboot.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
@@ -19,6 +21,9 @@ class SpringBootTestApplicationTests {
     @Autowired
     ApplicationContext ioc;
 
+    // 记录器
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @Test
     public void testHelloService () {
         boolean b = ioc.containsBean("helloService");
@@ -28,7 +33,21 @@ class SpringBootTestApplicationTests {
 
     @Test
     void contextLoads() {
+
+        /**
+         * 日志级别：
+         * 由低到高：trace < debug < info < warn < error
+         * 可以调整输出的日志级别：日志就只会在这个级别及以后的高级别生效
+         * SpringBoot 默认给我们使用的是 info 级别的
+         * 没有指定级别的就用SpringBoot默认规定的级别；也叫 root 级别
+         */
+        logger.trace("trace 级别的日志");
+        logger.debug("debug 级别的日志");
+        logger.info("info 级别的日志");
+        logger.warn("warn 级别的日志");
+        logger.error("error 级别的日志");
         System.out.println(person);
+
     }
 
 }
